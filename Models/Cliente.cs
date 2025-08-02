f8ab7dacf06ba2c0f83d17d1f9fd81835b7db8c3
Fred <fduartej@gmail.com>
2025-08-02 15:56:53 -0500

    subiendo nuevos cambios

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+
 namespace netdemo.Models;
 
 public class Cliente
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string? Nombre { get; set; }
+    [Required(ErrorMessage = "El apellido es obligatorio.")]
     public string? Apellido { get; set; }
+    [EmailAddress(ErrorMessage = "El correo electrónico no es válido.")]
     public string? Email { get; set; }
 }
