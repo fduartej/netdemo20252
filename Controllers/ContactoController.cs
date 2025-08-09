ec5d37035987eb4f5ce9f7431eba03ec71e9e125
edgvrz <edgarmanuel123497@gmail.com>
2025-08-09 14:36:20 -0500

    formulario de contacto

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using netdemo.Models;
+
+
+namespace netdemo.Controllers;
+
+public class ContactoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Enviar(Contacto contacto)
+    {
+        if (ModelState.IsValid)
+        {
+            // Aquí podrías guardar el contacto en una base de datos o enviar un correo electrónico
+            ViewData["Mensaje"] = "Contacto enviado correctamente.";
+            return View("Index");
+        }
+
+        // Si el modelo no es válido, se regresa a la vista con los errores
+        return View("Index", contacto);
+    }
+}
